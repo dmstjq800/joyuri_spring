dd08fc2e708cde623def0b90207ec97bbef6d3bb
eunseop <dmstjq800@naver.com>
2025-04-02 20:24:38 +0900

    1

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,24 @@
-#!bin/bash
+#!/bin/bash
+echo "Installing Spring Boot app..."
+
+# 디렉토리 준비
+mkdir -p /opt/springboot
+
+# JAR 이동
+cp /home/ec2-user/app/joyuri.jar /opt/springboot/app.jar
+
+# systemd 서비스 등록
+cat <<EOF > /etc/systemd/system/springboot.service
+[Unit]
+Description=Spring Boot App
+After=network.target
+
+[Service]
+User=ec2-user
+ExecStart=/usr/bin/java -jar /opt/springboot/app.jar
+SuccessExitStatus=143
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
